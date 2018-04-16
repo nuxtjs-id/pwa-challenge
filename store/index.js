c275133f36e4aa1faee05d02ddd00dbba49fba72
Ihsan <ihsanmchink@gmail.com>
2018-04-16 19:26:45 +0700

    Try to add server cacher with node-cache

--- store/index.js
+++ store/index.js
@@ -1,4 +1,6 @@
 import Vuex from 'vuex'
+const NodeCache = require( "node-cache" )
+const myCache = new NodeCache()
 
 const Store = () => {
   return new Vuex.Store({
@@ -14,32 +16,41 @@ const Store = () => {
     },
     mutations: {
         loadDataInit (state, data) {
+            state.dataInit = data
         },
     },
     actions: {
-        async nuxtServerInit ({state, commit}, {app, isDev, req, redirect}) {
+        nuxtServerInit ({state, commit}, {app, isDev, req, redirect}) {
             if (process.server) {
-                const hostParts = (req.headers.host || '').replace('.org', '').split('.')
-                console.log(req.connection.encrypted)
-                // if (hostParts.length === 2) {
-                //     if (hostParts[0] === 'www') return redirect(301, 'https://nuxtjs.org' + req.url)
-                //     commit('setLocale', hostParts[0])
-                // }
                 if (!state.datainit) {
-                    await app.$axios.$get('https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2Fwwwid')
-                        .then((res) => {
+                    myCache.get( "dataWWWID", function( err, value ){
+                        if( !err ){
                             let slug
                             let i
-                            for (i = 0; i < res.items.length; i++) {
-                                slug = res.items[i].title.toLowerCase().trim().replace(/[^0-9a-zA-Z]/g, '-')
-                                res.items[i].desc = ' ...'
-                                res.items[i].slug = slug
+                            if(value === undefined){
+                                app.$axios.$get('https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2Fwwwid')
+                                    .then((res) => {
+                                        myCache.set( "dataWWWID", res)
+                                        for (i = 0; i < res.items.length; i++) {
+                                            slug = res.items[i].title.toLowerCase().trim().replace(/[^0-9a-zA-Z]/g, '-')
+                                            res.items[i].desc = ' ...'
+                                            res.items[i].slug = slug
+                                        }
+                                        commit('loadDataInit', res)
+                                    })
+                                    .catch(e => {
+                                        console.log('E: ServerInit - ' + e.message)
+                                    })
+                            } else {
+                                for (i = 0; i < value.items.length; i++) {
+                                    slug = value.items[i].title.toLowerCase().trim().replace(/[^0-9a-zA-Z]/g, '-')
+                                    value.items[i].desc = ' ...'
+                                    value.items[i].slug = slug
+                                }
+                                commit('loadDataInit', value)
                             }
-                            state.dataInit = res
-                        })
-                        .catch(e => {
-                            console.log('E: ServerInit - ' + e.message)
-                        })
+                        }
+                    })
                 }
             }
         }
